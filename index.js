4a67385908984fb136ded33c3c1358deb978eb52
josepheastman <josephryan96@gmail.com>
2019-01-15 14:43:24 -0800

    Formatted

--- index.js
+++ index.js
@@ -14,127 +14,162 @@ const db = knex(knexConfig.development);
 // endpoints here
 
 // ** ZOOS **
-server.get('/api/zoos', (req, res) => {
-  db('zoos')
-  .then(zoos => {
-    res.status(200).json(zoos);
-  })
-  .catch(err => res.status(500).json({ error: "The zoo information could not be retrieved." }))
-})
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The zoo information could not be retrieved." })
+    );
+});
 
-server.get('/api/zoos/:id', (req, res) => {
-  db('zoos')
-  .where({ id: req.params.id })
-  .then(zoo => {
-    if (zoo) {
-      res.status(200).json(zoo)
-    } else {
-      res.status(404).json({ error: "Zoo not found" })
-    }
-  })
-  .catch(err => res.status(500).json({ error: "The zoo information could not be retrieved." }))
-})
+server.get("/api/zoos/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ error: "Zoo not found" });
+      }
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The zoo information could not be retrieved." })
+    );
+});
 
-server.post('/api/zoos', (req, res) => {
+server.post("/api/zoos", (req, res) => {
   const changes = req.body;
 
   db.insert(changes)
-  .into('zoos')
-  .then(ids => {
-    res.status(201).json(ids);
-  })
-  .catch(err => {
-    res.status(500).json({ error: "There was an error while saving the zoo to the database." });
-  })
-})
+    .into("zoos")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the zoo to the database."
+        });
+    });
+});
 
-server.delete('/api/zoos/:id', (req, res) => {
-  db('zoos')
-  .where({ id: req.params.id })
-  .del()
-  .then(count => {
-    if (count) {
-      res.status(200).json(count)
-    } else {
-      res.status(404).json({ error: "Zoo not found" })
-    }
-  })
-  .catch(err => res.status(500).json({ error: "The zoo could not be removed." }))
-})
+server.delete("/api/zoos/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ error: "Zoo not found" });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "The zoo could not be removed." })
+    );
+});
 
-server.put('/api/zoos/:id', (req, res) => {
+server.put("/api/zoos/:id", (req, res) => {
   const changes = req.body;
 
-  db('zoos')
-  .where({ id: req.params.id })
-  .update(changes)
-  .then(count => {
-    res.status(200).json(count)
-  })
-  .catch(err => res.status(500).json({ error: "The zoo information could not be modified." }));
-})
+  db("zoos")
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The zoo information could not be modified." })
+    );
+});
 
 // ** BEARS **
-server.get('/api/bears', (req, res) => {
-  db('bears')
-  .then(bears => {
-    res.status(200).json(bears);
-  })
-  .catch(err => res.status(500).json({ error: "The bear information could not be retrieved." }))
-})
+server.get("/api/bears", (req, res) => {
+  db("bears")
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The bear information could not be retrieved." })
+    );
+});
 
-server.get('/api/bears/:id', (req, res) => {
-  db('bears')
-  .where({ id: req.params.id })
-  .then(bear => {
-    if (bear) {
-      res.status(200).json(bear)
-    } else {
-      res.status(404).json({ error: "Bear not found" })
-    }
-  })
-  .catch(err => res.status(500).json({ error: "The Bear information could not be retrieved." }))
-})
+server.get("/api/bears/:id", (req, res) => {
+  db("bears")
+    .where({ id: req.params.id })
+    .then(bear => {
+      if (bear) {
+        res.status(200).json(bear);
+      } else {
+        res.status(404).json({ error: "Bear not found" });
+      }
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The Bear information could not be retrieved." })
+    );
+});
 
-server.post('/api/bears', (req, res) => {
+server.post("/api/bears", (req, res) => {
   const changes = req.body;
 
   db.insert(changes)
-  .into('bears')
-  .then(ids => {
-    res.status(201).json(ids);
-  })
-  .catch(err => {
-    res.status(500).json({ error: "There was an error while saving the bear to the database." });
-  })
-})
+    .into("bears")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the bear to the database."
+        });
+    });
+});
 
-server.delete('/api/bears/:id', (req, res) => {
-  db('bears')
-  .where({ id: req.params.id })
-  .del()
-  .then(count => {
-    if (count) {
-      res.status(200).json(count)
-    } else {
-      res.status(404).json({ error: "Bear not found" })
-    }
-  })
-  .catch(err => res.status(500).json({ error: "The bear could not be removed." }))
-})
+server.delete("/api/bears/:id", (req, res) => {
+  db("bears")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ error: "Bear not found" });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "The bear could not be removed." })
+    );
+});
 
-server.put('/api/bears/:id', (req, res) => {
+server.put("/api/bears/:id", (req, res) => {
   const changes = req.body;
 
-  db('bears')
-  .where({ id: req.params.id })
-  .update(changes)
-  .then(count => {
-    res.status(200).json(count)
-  })
-  .catch(err => res.status(500).json({ error: "The Bear information could not be modified." }));
-})
-
+  db("bears")
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The Bear information could not be modified." })
+    );
+});
 
 const port = 3300;
 server.listen(port, function() {
