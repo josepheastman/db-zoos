37c52a0b0b7ef8195edf03755f12d7e13b85c9d6
josepheastman <josephryan96@gmail.com>
2019-01-15 14:01:19 -0800

    Completed MVP endpoints

--- index.js
+++ index.js
@@ -1,12 +1,84 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const db = knex(knexConfig.development);
+
 // endpoints here
+
+// server.get('/api/zoos', (req, res) => {
+//   res.send('api working')
+// })
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos);
+  })
+  .catch(err => res.status(500).json({ error: "The zoo information could not be retrieved." }))
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({ id: req.params.id })
+  .then(zoo => {
+    if (zoo) {
+      res.status(200).json(zoo)
+    } else {
+      res.status(404).json({ error: "Zoo not found" })
+    }
+  })
+  .catch(err => res.status(500).json({ error: "The zoo information could not be retrieved." }))
+})
+
+server.post('/api/zoos', (req, res) => {
+  const changes = req.body;
+
+  db.insert(changes)
+  .into('zoos')
+  .then(ids => {
+    res.status(201).json(ids);
+  })
+  .catch(err => {
+    res.status(500).json({ error: "There was an error while saving the zoo to the database." });
+  })
+})
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({ id: req.params.id })
+  .del()
+  .then(count => {
+    if (count) {
+      res.status(200).json(count)
+    } else {
+      res.status(404).json({ error: "Zoo not found" })
+    }
+  })
+  .catch(err => res.status(500).json({ error: "The zoo could not be removed." }))
+})
+
+server.put('/api/zoos/:id', (req, res) => {
+  const changes = req.body;
+
+  db('zoos')
+  .where({ id: req.params.id })
+  .update(changes)
+  .then(count => {
+    res.status(200).json(count)
+  })
+  .catch(err => res.status(500).json({ error: "The zoo information could not be modified." }));
+})
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
